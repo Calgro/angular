57210927baba0d00d86ba6cd4d9098c341341b01
DominiqueC <DominiqueC@dominiqueCoope.CalgroM3.local.com>
2018-04-24 15:19:05 +0200

    Contractors List FE Complete. Testing Done

--- src/app/modules/contractors/contractorslist/contractorsList.component.ts
+++ src/app/modules/contractors/contractorslist/contractorsList.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Contractors } from '../../../models/contractors.model';
-// why call the contractorShort model if the contractors model calls it?
-import { ContractorShort } from '../../../models/contractorShort.model';
 import { ContractorsService } from '../../../services/contractors.service';
 import { FilterService } from '../../../services/filter.service';
 import { Router } from '@angular/router';
 
-
-// Do we need to call alertify.js to enable the alerts/message functionality? That's the HTTP codes that we return to the user?
 const alertify = require('alertify.js');
-
 
 @Component({
   selector: 'app-contractorslist',
@@ -17,27 +12,22 @@ const alertify = require('alertify.js');
   styleUrls: ['./contractorsList.component.css']
 })
 
-
-// I'm getting ContractorsListComponent from the "export class ContractorsService {" line in the contractors service, is that correct?
 export class ContractorsListComponent implements OnInit {
-//  We don't have the following 3 lines of code in the Orders Filter .ts ... Do I need to have this because we are creating a list?
-  contractors: Contractors = new Contractors(null);  // Is contractors a new initialized variable that inherits the properties of the Contractors model (which means it inherits the ContractorShort properties)?
-  contractorsLoaded = true;  // Initializing for line 51 !!! change to false once the backend is completed !!!
-  noContractors = true;  // Initializing for line 49    !!! change to false once the backend is completed !!!  *** Tested when manually setting it's value to true and false
-
+  contractors: Contractors = new Contractors(null);
+  contractorsLoaded = false;
+  noContractors = false;
 
 
   constructor(
-    // Must we create a variable for each service we use in the .ts ?
     private contractorsService: ContractorsService,
     private filterService: FilterService,
     private router: Router
-      )  { }
+  )  { }
 
 
 
   ngOnInit() {
-      // Load a list of contractors
+      // Load a list of all of the contractors
       this.contractorsService.contractorListChanged.subscribe(
           (contractors: Contractors) => {
               console.log(contractors);
@@ -49,6 +39,7 @@ export class ContractorsListComponent implements OnInit {
               this.contractorsLoaded = true;
            }
         );
+      this.contractorsService.fetchContractors(null, null, null, null, null);
   }
 
 }
