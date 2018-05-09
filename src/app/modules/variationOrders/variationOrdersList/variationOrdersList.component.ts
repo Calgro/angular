9d55c04972465958dc8fd29e429f7f13211d38ee
DominiqueC <DominiqueC@dominiqueCoope.CalgroM3.local.com>
2018-05-09 11:34:19 +0200

    Variation Orders List HTML WIP

--- src/app/modules/variationOrders/variationOrdersList/variationOrdersList.component.ts
+++ src/app/modules/variationOrders/variationOrdersList/variationOrdersList.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Projects } from '../../../models/projects.model';
+import { ProjectShort } from '../../../models/projectShort.model';
+import { Townships } from '../../../models/townships.model';
+import { TownshipShort } from '../../../models/townshipShort.model';
+import { Erven } from '../../../models/erven.model';
+import { ErfShort } from '../../../models/erfShort.model';
+import { PrivateUseAreas } from '../../../models/privateUseAreas.model';
+import { PrivateUseAreaShort } from '../../../models/privateUseAreaShort.model';
+import { Buildings } from '../../../models/buildings.model';
+import { BuildingShort } from '../../../models/buildingShort.model';
+
+import { ProjectsService } from '../../../services/projects.service';
+import { TownshipsService } from '../../../services/townships.service';
+import { ErvenService } from '../../../services/erven.service';
+import { PuaService } from '../../../services/pua.service';
+import { BuildingsService } from '../../../services/buildings.service';
+
+import { FilterService } from '../../../services/filter.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
