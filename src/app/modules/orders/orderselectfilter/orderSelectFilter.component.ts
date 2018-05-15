8d69c44cb4ef5935fb01729a0e7e5fee9a3127b2
DominiqueC <DominiqueC@dominiqueCoope.CalgroM3.local.com>
2018-05-15 15:57:40 +0200

    WIP
    Need to pull the user input through to the ts.

--- src/app/modules/orders/orderselectfilter/orderSelectFilter.component.ts
+++ src/app/modules/orders/orderselectfilter/orderSelectFilter.component.ts
@@ -1,4 +1,8 @@
+import { OrdersService } from '../../../services/orders.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+const alertify = require('alertify.js');
 
 @Component({
   selector: 'app-orderselectfilter',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderSelectFilter.component.css']
 })
 export class OrderSelectFilterComponent implements OnInit {
+  private orderNumber;
 
-  constructor() { }
+  constructor(
+    private ordersService: OrdersService,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+
+  loadOrderDetails(form: NgForm) {
+console.log('Testing ' this.orderNumber);
+
+//    this.ordersService.listMode = listMode;
+//    this.ordersService.orderID = orderID;
+    this.router.navigate(['/admin/orders/detail']);
   }
-
 }
