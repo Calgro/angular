b3c11b4f048b06a6b5994d68e68997941d4cf47f
DominiqueC <DominiqueC@dominiqueCoope.CalgroM3.local.com>
2018-05-15 11:22:28 +0200

    Calgro M3 Connect Complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,41 +16,38 @@ const alertify = require('alertify.js');
 })
 export class LoginComponent {
 
+  SignInData = {
+    'name': null,
+    'surname': null,
+    'cell': null,
+    'email': null,
+  };
+
   constructor(private http: HttpClient, private router: Router, private authService: AuthService, private menusService: MenusService) { }
-  private login = require('../images/login.jpg');
+  private login = require('../images/CalgroM3Connect.jpg');
+  private logo = require('../images/logo.png');
 
   onSignIn(form: NgForm) {
     const name    = form.value.name;
     const surname = form.value.surname;
     const cell    = form.value.cell;
     const email   = form.value.email;
-    const formData: FormData = new FormData();
-    formData.append('name', name);
-    formData.append('surname', surname);
-    formData.append('cell', cell);
-    formData.append('email', email);
 
     if (name === '') {
       alertify.error('First Name is required');
     } else if (surname === '') {
         alertify.error('Surname is required');
-    } else if (cell === '') {
-        alertify.error('Cell Number is required');
     } else if ((cell.toString()).length !== 9) {
         alertify.error('Please enter a valid Cell Number');
     } else if (email === '') {
         alertify.error('Email Address is required');
     } else {
-      
-      
-console.log(formData[0]);
-console.log(name);
-console.log(surname);
-console.log(cell);
-console.log(email);
-            
-            
-      this.http.post('https://www.calgrois.co.za/api/v1/calgroM3Connect', formData).subscribe(
+        this.SignInData.name    = name;
+        this.SignInData.surname = surname;
+        this.SignInData.cell    = '0' + cell.toString();
+        this.SignInData.email   = email;
+
+      this.http.post('https://www.calgrois.co.za/api/v1/calgroM3Connect', this.SignInData).subscribe(
         resp => {
           console.log(resp);
           this.authService.setToken(resp['JWT']);
